a53707576a220beac539ab62379119a410378f00
Geodego <geoffroydegournay@hotmail.com>
2022-08-03 17:31:06 +0200

    post request added to API

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import logging
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 logging.basicConfig(level=logging.INFO, format="%(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger()
@@ -15,12 +16,24 @@ if "DYNO" in os.environ and os.path.isdir(".dvc"):
 
     pull_err = os.system("dvc pull")
     if pull_err != 0:
-        logger.warning(f" New dvc pull failed, error: {pull_err}")
         exit(f"dvc pull failed, error {pull_err}")
     else:
         logger.warning("DVC Pull worked.")
     logger.warning('removing dvc files')
     os.system("rm -r .dvc .apt/usr/lib/dvc")
+
+
+class Item(BaseModel):
+    a: int
+    b: int
+
+    class Config:
+        schema_extra = {
+            "example": {
+                'a': 2,
+                'b': 5
+            }
+        }
 
 
 def create_data():
@@ -39,10 +52,39 @@ async def api_columns():
     In this basic example we read data.csv and return the columns of the data frame. This requires dvc pull to be
     executed on the Heroku VM.
     """
+    logger.warning("entering GET request")
     logger.warning(f'error status of dvc pull: {pull_err}')
     df = pd.read_csv('data.csv')
     return {"columns of data.csv": str(list(df.columns))}
 
 
+def post_tool(predict_body: Item) -> Item:
+    """
+    Basic transformation of the input of the post request.
+    """
+    df = pd.read_csv('data.csv')[['a', 'b']].iloc[0]
+    output = predict_body.copy()
+    output.a += df['a']
+    output.b += df['b']
+    return output
+
+
+@app.post("/predict")
+async def predict(predict_body: Item):
+    """
+    In this basic example we read data.csv and return a dictionary with the values of its first row added
+    to predict_body.
+    This requires dvc pull to be executed on the Heroku VM.
+    """
+    logger.warning("entering POST request")
+    logger.warning(f'error status of dvc pull: {pull_err}')
+    output = post_tool(predict_body)
+
+    return output
+
+
 if __name__ == '__main__':
-    create_data()
+    item = Item(**{'a': 1, 'b': 2})
+    item1 = post_tool(item)
+    print(item1)
+    #create_data()
